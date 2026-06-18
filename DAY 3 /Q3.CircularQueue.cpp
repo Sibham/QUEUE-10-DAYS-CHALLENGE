3406c5520c4c6a46863340ecf822673b44977381
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-06-18 17:58:28 +0530

    Implement CircularQueue class with enqueue and dequeue

---
+++ DAY 3 /Q3.CircularQueue.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+
+class CircularQueue {
+    int *arr;
+    int front;
+    int rear;
+    int size;
+
+public:
+    CircularQueue(int n) {
+        size = n;
+        arr = new int[size];
+        front = rear = -1;
+    }
+
+    bool enqueue(int value) {
+        // Queue full condition
+        if ((front == 0 && rear == size - 1) || (rear == (front - 1 + size) % size)) {
+            return false;
+        } else if (front == -1) { // First element
+            front = rear = 0;
+        } else if (rear == size - 1 && front != 0) { // Wrap around
+            rear = 0;
+        } else { // Normal case
+            rear++;
+        }
+        arr[rear] = value;
+        return true;
+    }
+
+    int dequeue() {
+        if (front == -1) {
+            return -1; // Queue empty
+        }
+        int ans = arr[front];
+        arr[front] = -1;
+
+        if (front == rear) { // Single element
+            front = rear = -1;
+        } else if (front == size - 1) { // Wrap around
+            front = 0;
+        } else {
+            front++;
+        }
+        return ans;
+    }
+};
